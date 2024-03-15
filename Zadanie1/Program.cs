a36c83089aeddef2e27a134096d100609441aa29
s27496 <s27496@pjwstk.edu.pl>
2024-03-15 17:26:45 +0100

    dodano max

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -9,9 +9,24 @@
     return sum / input.Length;
 }
 
+static int Max(int[] input)
+{
+    var max = int.MinValue;
+    for (var i = 0; i < input.Length; i++)
+    {
+        if (input[i] > max)
+        {
+            max = input[i];
+        }
+    }
+
+    return max;
+}
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("zmiana 1");
 Console.WriteLine("zmiana 2");
 Console.WriteLine("zmiana 3");
 var data = new[] { 1, 2, 3, 4 };
 Console.WriteLine(Average(data));
+Console.WriteLine(Max(data));
