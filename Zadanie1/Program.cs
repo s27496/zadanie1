79547f318ddf9ffd68a2617a997fff75a023f1cb
s27496 <s27496@pjwstk.edu.pl>
2024-03-15 17:12:13 +0100

    dodano średnią

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,6 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿static double Average(int[] input)
+{
+    var sum = 0.0;
+    for (var i = 0; i < input.Length; i++)
+    {
+        sum += input[i];
+    }
+
+    return sum / input.Length;
+}
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("zmiana 1");
 Console.WriteLine("zmiana 2");
 Console.WriteLine("zmiana 3");
+var data = new[] { 1, 2, 3, 4 };
+Console.WriteLine(Average(data));
